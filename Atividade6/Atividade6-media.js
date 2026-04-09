31ffa3afe602380da0413334bba710ae68ae2dd6
Jul1aF <143774795+Jul1aF@users.noreply.github.com>
2026-04-08 21:08:52 -0300

    Delete Atividade6 directory

--- Atividade6/Atividade6-media.js
+++
@@ -1,9 +0,0 @@
-let nome = prompt("Digite o nome do aluno: ");
-let nota1 = parseFloat(prompt("Digite a nota 1: "));
-let nota2 = parseFloat(prompt("Digite a nota 2: "));
-let nota3 = parseFloat(prompt("Digite a nota 3: "));
-let nota4 = parseFloat(prompt("Digite a nota 4: "));
-let resultado = (nota1+nota2+nota3+nota4)/4;;
-
-alert("Aluno: " + nome + "\n Média: " + resultado.toFixed(2));
-
