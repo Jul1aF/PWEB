e09f4564f45cb05cae7dfe841cd992b94b6532cb
Jul1aF <143774795+Jul1aF@users.noreply.github.com>
2026-04-08 21:20:33 -0300

    Add files via upload

---
+++ Atividade6/Atividade6-operacoes.js
@@ -0,0 +1,14 @@
+let num1 = parseFloat(prompt("Digite o numero 1: "));
+let num2 = parseFloat(prompt("Digite o numero 2: "));
+
+
+let resultadoSoma = num1 + num2;
+let resultadoSub = num1 - num2;
+let resultadoProd = num1 * num2;
+let resultadoDiv = num1/num2;
+let resultadoRest = num1%num2;
+
+alert("Soma: " + resultadoSoma + "\nSubtração: " + resultadoSub + "\nMultiplicação: " + resultadoProd + "\nDivisão: " + resultadoDiv + "\nResto: " + resultadoRest);
+
+
+
